36f57aa296361ed5fc70ba98505ef248058d366a
Wes Baker <wes@wesbaker.com>
2019-08-11 14:31:58 -0400

    Cleaning copypasta from eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,8 @@ module.exports = {
   env: {
     es6: true,
     node: true,
-    "jest/globals": true
   },
-  plugins: ["jest"],
-  extends: ["eslint:recommended", "plugin:jest/recommended"],
-  parserOptions: {
-    sourceType: "module"
-  },
+  extends: ["eslint:recommended"],
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
